5ce15ed3b2f94aa777438ea9cf5c095de17e0a03
xuming <vip6ming@126.com>
2014-09-10 21:30:59 +0800

    card上传页面

---
+++ src/com/lengtoo/impress/web/struts1/action/web/card/UploadLengtooCard.java
@@ -0,0 +1,25 @@
+/**
+* <p>Title: UploadLengtooCard.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2014</p>
+* <p>Company: ColdWorks</p>
+* @author xuming
+* @date 2014-9-10
+* @version 1.0
+*/
+package com.lengtoo.impress.web.struts1.action.web.card;
+
+import org.apache.struts.action.Action;
+
+/**
+ * <p>Title: UploadLengtooCard.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2014</p>
+ * <p>Company: ColdWorks</p>
+ * @author xuming
+ * @date 2014-9-10
+ * Email: vip6ming@126.com
+ */
+public class UploadLengtooCard extends Action{
+
+}
